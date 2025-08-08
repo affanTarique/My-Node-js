0bc8d9a25890c8804c88a87065b65bd35dc02eb9
affanTarique <reply2affankhan@gmail.com>
2025-08-08 17:30:43 +0530

    js

--- 01 Basic-JS/index.js
+++ 01 Basic-JS/index.js
@@ -1,9 +1,15 @@
-alert();
+
 //fundamental of javascript
 // arrays and object
 //function and return
 //async js coding
+// forEach map filter find indexof
+var arr1 =[1,2,3,4,5,6, "hey", {}, true, false]
 
-var arr =[1,2,3,4,5,6, "hey", {}, true, false]
+var arr = [1,2,3,4];
+arr.forEach(function(val) {
+    console.log(val + " hello");
+})
 
-// foreach map filter
+
+
