74f0d4bbbc489f21f7c0bd34f61d80b49cb2d296
affanTarique <reply2affankhan@gmail.com>
2025-08-13 23:34:43 +0530

    node

--- 02 install-node-js/index.js
+++ 02 install-node-js/index.js
@@ -27,3 +27,8 @@ fs.rename('taimoor.txt', "kabbu.txt", function(err){
     if(err)console.error(err);
     else console.log("file renamed successfully") 
 })
+
+fs.copyFile('kabbu.txt', 'kabbu2.txt', function(err){
+    if(err) console.error(err);
+    else console.log("file copied successfully")
+})
