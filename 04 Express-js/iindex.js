4e04da58606930b0979c96df675d5bce82ccb10e
affanTarique <reply2affankhan@gmail.com>
2025-08-14 15:35:17 +0530

    express

--- 04 Express-js/iindex.js
+++ 04 Express-js/iindex.js
@@ -1,5 +1,11 @@
 //express.js framework
 //introduction to express.js
+//----express js ek npm package hai jo ki node.js ke upar chalta hai
+//--- expressjs ek framework hai
+//--- express.js ko use karke aap web application develop kar sakte ho-----manges everything from receiving the request and giving the response-------
+
+
+
 //setting up a  basic express application
 //routing
 //middlewere
