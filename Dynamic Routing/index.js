11dbeddad6f9c5c6389cbbf980c94eebb90a321a
affanTarique <reply2affankhan@gmail.com>
2025-09-12 23:59:20 +0530

    route

--- Dynamic Routing/index.js
+++ Dynamic Routing/index.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: ture})); //yeh two lines form ko ham use kar ske uske liye likhe hai//
 
-const expr
+app.get("/", function(req, res) { //route setup krre hai//
+    res.send("chal raha hai chaalu hai");
+
+});
+
+app.listen(3000,function() {
+    console.log("server is running on port 4000");
+})
